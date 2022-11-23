2ee378a19b40cf7aed9334b0e7f30690553eb475
Chango <schang09032003@gmail.com>
2022-11-23 02:47:00 +0000

    Feature : Finished Variable Section

--- Syntax/console.js
+++ Syntax/console.js
@@ -18,3 +18,59 @@ console.log("Teaching the world how to code".length);
 console.log('Codecademy'.toUpperCase());
 console.log('    Remove whitespace    '.trim());
 
+//Built-in Objects
+console.log(Math.floor(Math.random()*100)); //Random = random number from 0 - 1 and Floor rounds down
+console.log(Math.ceil(43.8)); //Finds smallest integer that is larger than decimal number
+console.log(Number.isInteger(2017)); //returns true or false depending on whether object is integer or not
+
+//Variable (const)
+var myName = 'Sean';
+console.log(myName);
+
+//Variable (dynamic)
+let year = 2022;
+year = 2023;
+console.log(year); //Output: 2023
+
+//Variable (const)
+const name = 'Sean';
+console.log(name);
+//name = 'not Sean' wpi;d caise a type error
+
+//Mathematical Assignment
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+levelUp += 5;
+powerLevel -= 100;
+multiplyMe *= 11;
+quarterMe /= 4;
+
+console.log(levelUp);
+console.log(powerLevel);
+console.log(multiplyMe);
+console.log(quarterMe);
+
+//Increment + Decrement
+let gainedDollar = 3;
+let lostDollar = 50;
+gainedDollar++;
+lostDollar--;
+
+//String Concatenation with Variables
+let favoriteAnimal = 'cat';
+console.log('My favorite animal: ' + favoriteAnimal);
+
+//String Interpolation
+let firstName = 'Sean';
+console.log(`My name is ${firstName}`); //replaces items inside ${} with Variables
+
+//typeof operator
+let newVariable = 'Playing around with typeof';
+console.log(typeof newVariable); //String
+newVariable = 9;
+console.log(typeof newVariable); //number
+
+
