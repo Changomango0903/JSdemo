c766e8b2ba7ec07a0a67bf8a94a61badd154cf16
Chango <schang09032003@gmail.com>
2022-11-23 01:26:34 +0000

    Initial Commit

---
+++ Syntax/console.js
@@ -0,0 +1,20 @@
+console.log(5); //Integer Type
+console.log('JavaScript'); //String Type
+console.log(20.49); //Number Type
+
+//Arithmetic Operands
+console.log(3.5 + 19);
+console.log(2022 - 1969);
+console.log(65/240);
+console.log(0.2708*100);
+
+//String Concatenation
+console.log("Hello" + "World");
+
+//Properties
+console.log("Teaching the world how to code".length);
+
+//Methods
+console.log('Codecademy'.toUpperCase());
+console.log('    Remove whitespace    '.trim());
+
